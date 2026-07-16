c074ad373416bebd727a9729ba31af3b8086c45d
1tsDan <2kupwhite@gmail.com>
2026-07-16 01:39:32 +0100

    refac: switch from threads to async connection managment

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use tokio::net::tcp::OwnedWriteHalf;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::Mutex;
+use std::sync::Arc;
 use std::collections::HashMap;
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
-use std::thread;
 
 struct FireKeeper {
-    streams: HashMap<usize, TcpStream>,
+    streams: HashMap<usize, OwnedWriteHalf>,
     next_id: usize,
 }
 
@@ -17,62 +18,63 @@ impl FireKeeper {
         }
     }
 
-    fn recognize(&mut self, stream: TcpStream) -> usize {
+    async fn recognize(&mut self, stream: OwnedWriteHalf) -> usize {
         let id = self.next_id;
         self.next_id += 1;
-        
+
         self.streams.insert(id, stream);
-        self.warn(&format!("{} entered the shrine!", id));
+        self.warn(&format!("{} entered the shrine!", id)).await;
 
         id
     }
 
-    fn forget(&mut self, id: usize) {
+    async fn forget(&mut self, id: usize) {
         self.streams.remove(&id);
-        self.warn(&format!("{} left the shrine!", id));
+        self.warn(&format!("{} left the shrine!", id)).await;
     }
 
-    fn warn(&mut self, content: &str) {
+    async fn warn(&mut self, content: &str) {
         for (_, stream) in &mut self.streams {
             let msg = format!("Keeper: {}\n", content);
-            stream.write_all(msg.as_bytes()).unwrap();
+            stream.write_all(msg.as_bytes()).await.unwrap();
         }
 
         println!("Keeper: {}", content);
     }
 
-    fn broadcast(&mut self, content: &str, from: usize) {
+    async fn broadcast(&mut self, content: &str, from: usize) {
         for kp in &mut self.streams {
             if from == *kp.0 {
                 continue;
             }
-            
+
             let msg = format!("{}: {}\n", from, content.trim_end());
-            kp.1.write_all(msg.as_bytes()).unwrap();
+            kp.1.write_all(msg.as_bytes()).await.unwrap();
         }
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     let keeper = Arc::new(Mutex::new(FireKeeper::new()));
 
     loop {
-        let (stream, _addr) = listener.accept().unwrap();
+        let (stream, _addr) = listener.accept().await.unwrap();
         let fk = Arc::clone(&keeper);
-        thread::spawn(move || {
-            let reader_stream = stream.try_clone().unwrap();
-            let id = fk.lock().unwrap().recognize(stream);
+        tokio::spawn(async move {
+            let (reader_stream, write_stream) = stream.into_split();
+            let id = fk.lock().await.recognize(write_stream).await;
             let mut reader = BufReader::new(reader_stream);
             loop {
                 let mut ln = String::new();
-                let len = reader.read_line(&mut ln).unwrap();
+                let len = reader.read_line(&mut ln).await.unwrap();
                 if len == 0 {
-                    fk.lock().unwrap().forget(id);
+                    fk.lock().await.forget(id).await;
                     break;
                 }
                 println!("{}: {}", id, ln.trim_end());
-                fk.lock().unwrap().broadcast(&ln, id);
+                fk.lock().await.broadcast(&ln, id).await;
             }
         });
     }
